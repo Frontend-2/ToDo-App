0bf850392b4ebd5a9540021b5ea02bb6cfb0b631
ctd-frontend <92804884+ctd-frontend@users.noreply.github.com>
2022-04-10 20:30:46 -0300

    path

--- scripts/signup.js
+++ scripts/signup.js
@@ -59,7 +59,7 @@ window.addEventListener('load', function () {
                     localStorage.setItem('jwt', JSON.stringify(data.jwt));
 
                     //redireccionamos a la página
-                    location.href = '/mis-tareas.html';
+                    location.replace('/mis-tareas.html');
                 }
                 
             }).catch(err => {
