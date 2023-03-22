754867efa9fd659bac36c69bc070feb046181cfa
ctd-frontend <92804884+ctd-frontend@users.noreply.github.com>
2023-03-21 21:33:04 -0300

    Update signup.js

--- scripts/signup.js
+++ scripts/signup.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function () {
     const apellido = document.querySelector('#inputApellido');
     const email = document.querySelector('#inputEmail');
     const password = document.querySelector('#inputPassword');
-    const url = 'https://ctd-todo-api.herokuapp.com/v1';
+    const url = 'https://todo-api.ctd.academy';
 
     /* -------------------------------------------------------------------------- */
     /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
